17a235a397e38191237ce26da5153b6585cb8fa2
gedenteen <Razumow.dmitrij@yandex.ru>
2025-02-28 22:06:24 +0500

    Implement method for sending robots to investigate

--- Assets/_Project/Scripts/Gameplay/ResourcesManager.cs
+++ Assets/_Project/Scripts/Gameplay/ResourcesManager.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Threading;
 using UnityEngine;
 using UniRx;
 using NaughtyAttributes;
+using Cysharp.Threading.Tasks;
 
 public class ResourcesManager : MonoBehaviour
 {
@@ -10,6 +13,36 @@ public class ResourcesManager : MonoBehaviour
     public ReactiveProperty<int> CountOfDays = new ReactiveProperty<int>(1);
     public ReactiveProperty<int> Wood = new ReactiveProperty<int>(50);
     public ReactiveProperty<int> Coal = new ReactiveProperty<int>(30);
+
+    [SerializeField] private float _minTimeForInvestigate = 1f;
+    [SerializeField] private float _maxTimeForInvestigate = 3f;
+
+    private readonly object _lock = new object();
+
+    [Button]
+    public async UniTask SendRobotToInvestigate()
+    {
+        lock (_lock)
+        {
+            if (CountOfAvailableRobots.Value <= 0)
+            {
+                Debug.Log("ResourcesManager: SendRobotToInvestigate: No available robots to send");
+                return;
+            }
+            CountOfAvailableRobots.Value--;
+        }
+        Debug.Log("ResourcesManager: SendRobotToInvestigate: Robot sent to investigate");
+        
+        // Дальше идёт асинхронная логика
+        float waitTime = UnityEngine.Random.Range(_minTimeForInvestigate, _maxTimeForInvestigate);
+        await UniTask.Delay(TimeSpan.FromSeconds(waitTime));
+
+        lock (_lock)
+        {
+            CountOfAvailableRobots.Value++;
+        }
+        Debug.Log("ResourcesManager: SendRobotToInvestigate: Robot returned from investigation");
+    }
 
     public void ChangeCountOfEnergy(int summand)
     {
