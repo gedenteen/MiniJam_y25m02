410958eee859e2ec1655e5e670dc7b42affda311
gedenteen <Razumow.dmitrij@yandex.ru>
2025-03-02 19:29:41 +0500

    Update GameLogsController

--- Assets/_Project/Scripts/Gameplay/GameLogsController.cs
+++ Assets/_Project/Scripts/Gameplay/GameLogsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UniRx;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
 
 public class GameLogsController : MonoBehaviour
 {
     [Header("References to my objects")]
     [SerializeField] private GameObject _holderForLogs;
+    [SerializeField] private ScrollRect _scrollRect;
 
     [Header("References to other objects")]
     [SerializeField] private ResourcesManager _resourcesManager;
@@ -18,6 +21,7 @@ public class GameLogsController : MonoBehaviour
 
     [Header("Parameters")]
     [SerializeField] private float _heightFor1RowOfText = 21f;
+    [SerializeField] private int _symbolsInOneLine = 40;
 
     private void Awake()
     {
@@ -26,11 +30,10 @@ public class GameLogsController : MonoBehaviour
             if (newValue == 1) 
             {
                 GameLog gameLog = Instantiate(_prefabGameLog, _holderForLogs.transform);
-                gameLog.RectTransform.sizeDelta = new Vector2(
-                    gameLog.RectTransform.sizeDelta.x, 
-                    _heightFor1RowOfText * 3);
                 gameLog.TextMesh.text = "The robot has discovered a forest. I can cut down trees " +
                     "and burn the wood to generate energy.";
+                SetAdaptiveHeightOfRectTransform(gameLog);
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
 
@@ -39,12 +42,11 @@ public class GameLogsController : MonoBehaviour
             if (newValue == 1) 
             {
                 GameLog gameLog = Instantiate(_prefabGameLog, _holderForLogs.transform);
-                gameLog.RectTransform.sizeDelta = new Vector2(
-                    gameLog.RectTransform.sizeDelta.x, 
-                    _heightFor1RowOfText * 3);
                 gameLog.TextMesh.text = $"I collected wood. By burning every " +
                     $"{_gameplayConfig.AmountOfWoodToBurn} units of wood, I will gain " +
                     $"{_gameplayConfig.AmountOfEnergyAfterBurningWood} units of energy.";
+                SetAdaptiveHeightOfRectTransform(gameLog);
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
         
@@ -58,6 +60,7 @@ public class GameLogsController : MonoBehaviour
                     _heightFor1RowOfText * 2);
                 gameLog.TextMesh.text = "The robot has discovered a coal deposit. Coal will " +
                     "provide more energy than wood.";
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
 
@@ -66,14 +69,13 @@ public class GameLogsController : MonoBehaviour
             if (newValue == 1) 
             {
                 GameLog gameLog = Instantiate(_prefabGameLog, _holderForLogs.transform);
-                gameLog.RectTransform.sizeDelta = new Vector2(
-                    gameLog.RectTransform.sizeDelta.x, 
-                    _heightFor1RowOfText * 5);
                 gameLog.TextMesh.text = $"I collected coal. By burning every " +
                     $"{_gameplayConfig.AmountOfCoalToBurn} units of wood, I will gain " +
                     $"{_gameplayConfig.AmountOfEnergyAfterBurningCoal} units of energy. " +
                     $"Burning coal is more efficient than burning wood, but on most planets, " +
                     $"coal is rarer than trees.";
+                SetAdaptiveHeightOfRectTransform(gameLog);
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
         
@@ -88,6 +90,7 @@ public class GameLogsController : MonoBehaviour
                 gameLog.TextMesh.text = "The robot has discovered a silicon deposit. If I have " +
                     "enough silicon and metals, I will be able to create more robots and build " +
                     "solar panels.";
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
         
@@ -98,9 +101,25 @@ public class GameLogsController : MonoBehaviour
                 GameLog gameLog = Instantiate(_prefabGameLog, _holderForLogs.transform);
                 gameLog.RectTransform.sizeDelta = new Vector2(
                     gameLog.RectTransform.sizeDelta.x, 
-                    _heightFor1RowOfText * 2);
+                    _heightFor1RowOfText * 3);
                 gameLog.TextMesh.text = "The robot has discovered a deposit of various metals. " +
                     "Progress should speed up now.";
+                ScrollToBottom().Forget();
+            }
+        }).AddTo(this);
+        
+        _resourcesManager.PropertiesMetals.ExtractedResources.Subscribe(newValue =>
+        {
+            if (newValue == 1) 
+            {
+                GameLog gameLog = Instantiate(_prefabGameLog, _holderForLogs.transform);
+                gameLog.RectTransform.sizeDelta = new Vector2(
+                    gameLog.RectTransform.sizeDelta.x, 
+                    _heightFor1RowOfText * 4);
+                gameLog.TextMesh.text = "Now I have various metals. To create a robot, I will " +
+                    $"need {_gameplayConfig.AmountOfSiliconForCreateRobot} unit of silicon and " +
+                    $"{_gameplayConfig.AmountOfMetalsForCreateRobot} units of metals.";
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
         
@@ -115,7 +134,23 @@ public class GameLogsController : MonoBehaviour
                 gameLog.TextMesh.text = "I have created an additional robot. The more robots " +
                     "there are, the faster the terrain will be explored and resources " +
                     "will be gathered.";
+                ScrollToBottom().Forget();
             }
         }).AddTo(this);
+    }
+
+    private void SetAdaptiveHeightOfRectTransform(GameLog gameLog)
+    {
+        int textLength = gameLog.TextMesh.text.Length;
+        float countOfRows = Mathf.CeilToInt((float)textLength / _symbolsInOneLine);
+        gameLog.RectTransform.sizeDelta = new Vector2(
+                    gameLog.RectTransform.sizeDelta.x, 
+                    countOfRows * _heightFor1RowOfText);
+    }
+
+    private async UniTask ScrollToBottom()
+    {
+        await UniTask.Yield(PlayerLoopTiming.Update);
+        _scrollRect.verticalNormalizedPosition = 0f;
     }
 }
